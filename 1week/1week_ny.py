893d88fc9b4b19aef06996e539a7682efd8eec2d
nang-e <yeanvely@naver.com>
2021-09-13 22:59:54 +0900

    @nang-e

---
+++ 1week/1week_ny.py
@@ -0,0 +1,22 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.11.5
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+def solution(price, money, count):
+    tot = (price + count*price)*count/2
+    answer = (tot-money)
+    return(0) if answer <= 0 else int(answer)
+
+
+solution(3,20,4)
