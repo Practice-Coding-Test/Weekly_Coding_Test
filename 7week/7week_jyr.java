8066582f1fcab3dee40290513a0c9cbd26a8dac1
unknown <winston1214@naver.com>
2021-09-17 20:12:12 +0900

    commit

---
+++ 7week/7week_jyr.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] enter, int[] leave) {
+        int[] answer = new int[enter.length];
+
+        Set<Integer> room = new HashSet<>();
+        int enterIdx = 0;
+        int leaveIdx = 0;
+        int endIdx = enter.length;
+
+        while(enterIdx < endIdx || leaveIdx < endIdx ) {
+            // 퇴실하는 사람: 방에 남은 사람들 수 만큼 만남 += room.size()-1
+            // 남은 사람: 퇴실하는 사람 만남 += 1
+            if (room.contains(leave[leaveIdx])) {
+                answer[leave[leaveIdx] - 1] += room.size() - 1;
+                room.remove(leave[leaveIdx]);
+
+                Iterator<Integer> iter = room.iterator();
+                while (iter.hasNext()) {
+                    answer[iter.next() - 1]++;
+                }
+
+                leaveIdx++;
+            } else {
+                room.add(enter[enterIdx]);
+                enterIdx++;
+            }
+        }
+        return answer;
+    }
+}
---
+++ 7week/7week_ny.py
@@ -0,0 +1,44 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.11.5
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+import numpy as np
+from random import *
+
+start = 1
+end = 10
+
+enter, leave = list(range(start, end)), list(range(start, end))
+np.random.shuffle(enter)
+np.random.shuffle(leave)
+print('enter: ', enter)
+print('leave: ', leave)
+
+
+def solution(enter, leave):
+    answer = []
+    for i in range(1,len(enter)+1):
+        last_idx = []
+        for j in range(1,len(enter)+1):
+            if (enter.index(i) < enter.index(j)) and (leave.index(i) > leave.index(j)):
+                last_idx.append(j)
+            if (enter.index(i) > enter.index(j)) and (leave.index(i) < leave.index(j)):
+                last_idx.append(j)
+
+        answer.append(len(last_idx))
+    return(answer)
+    
+
+
+solution(enter,leave)
