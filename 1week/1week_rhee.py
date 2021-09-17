f5cbea86a67ea531af130d1585ea487e93178e5d
unknown <winston1214@naver.com>
2021-09-17 20:06:45 +0900

    commit

---
+++ 1week/1week_rhee.py
@@ -0,0 +1,9 @@
+def solution(price, money, count):
+    money = money - price * count * (count + 1) / 2
+
+    if money >= 0:
+        answer = 0
+    else:
+        answer = -1 * money
+
+    return answer
