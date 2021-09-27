c75de846c223b20134e68385259ec8ca7bf29ff6
SK-jeong <skjeong913@ewhain.net>
2021-09-28 02:08:02 +0900

    SK-jeong

---
+++ 8week/8week_jsk.py
@@ -0,0 +1,18 @@
+def solution(sizes):
+    width = 0
+    height = 0
+    for size in sizes:
+        # set larger element as width, smaller as height
+        if size[0] >= size[1]:
+            w = size[0]
+            h = size[1]
+        else:
+            w = size[1]
+            h = size[0]
+        # update max width & height
+        if width < w:
+            width = w
+        if height < h:
+            height = h
+    answer = width*height
+    return answer
