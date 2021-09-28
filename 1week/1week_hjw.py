2b55a8dbdffba1bd8f7289efdf843a188745e4f3
sweetperssimmon <ghdwl1103@naver.com>
2021-09-28 14:41:07 +0900

    @sweetpersimmon

---
+++ 1week/1week_hjw.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def solution(price, money, count):
+    answer = 0
+    for i in range(1, count+1):
+        answer += price*i
+    return answer-money if (answer - money) > 0 else 0
+
