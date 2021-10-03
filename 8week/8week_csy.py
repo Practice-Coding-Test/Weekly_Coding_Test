314530de50106c95f6f19d65057714bf524a1fff
sssuyeon <87705722+sssuyeon@users.noreply.github.com>
2021-10-03 22:19:30 +0900

    Delete 8week_csy.py

--- 8week/8week_csy.py
+++
@@ -1,9 +0,0 @@
-def solution(sizes):
-    weight = 0
-    height = 0
-    
-    for w,h in sizes:
-        weight = max(weight, w, h) # 큰 것 중 가장 큰 것
-        height = max(height, min(w, h)) # 작은 것 중 가장 큰 것
-        
-    return weight*height
