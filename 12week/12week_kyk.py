bddacdaf82cb162b1f23402f9fc7aaae81eca457
yun kyung <dbsrud821@g.hongik.ac.kr>
2021-10-30 17:56:14 +0900

    @yeclairer
    @yeclairer

---
+++ 12week/12week_kyk.py
@@ -0,0 +1,23 @@
+from itertools import permutations
+def solution(k, dungeons):
+    n,answer= len(dungeons),0
+    item = [i for i in range(n)]
+    items = list(permutations(item, n))
+    dungeons.sort(key= lambda x:x[0], reverse= True)
+
+    for item in items:
+        power, temp= k, 0
+        for idx in item:
+            dungeon= dungeons[idx]
+            need, used = dungeon[0], dungeon[1]
+
+            if need <= power:
+                power -= used
+                temp += 1
+            else:
+                break
+            answer= max(answer,temp)
+            if answer == n:
+                return n
+
+    return answer
